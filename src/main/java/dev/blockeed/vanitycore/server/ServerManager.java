d66800e9abe834f5240c9f9215a867c89052c027
peterherczku <peterherczku915@gmail.com>
2023-09-30 15:13:24 +0200

    added getServers to ServerManager

--- src/main/java/dev/blockeed/vanitycore/server/ServerManager.java
+++ src/main/java/dev/blockeed/vanitycore/server/ServerManager.java
@@ -6,6 +6,8 @@ import dev.blockeed.vanitycore.server.objects.GameServer;
 import dev.blockeed.vanitycore.server.objects.LobbyServer;
 import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 @AllArgsConstructor
@@ -28,6 +30,32 @@ public class ServerManager {
                     callback.accept(GameServer.fromJson(jsonObject));
                     break;
             }
+        }));
+    }
+
+    public void getServers(Consumer<List<VanityServer>> callback) {
+        coreAPI.getRedisManager().getList("servers", (jsonObjects -> {
+
+            List<VanityServer> returnList = new ArrayList<>();
+
+            jsonObjects.forEach(jsonObject -> {
+                VanityServer.Type type = VanityServer.Type.valueOf(jsonObject.getString("type").toUpperCase());
+
+                switch (type) {
+                    case BUNGEE:
+                        returnList.add(BungeeServer.fromJson(jsonObject));
+                        break;
+                    case LOBBY:
+                        returnList.add(LobbyServer.fromJson(jsonObject));
+                        break;
+                    case GAME:
+                        returnList.add(GameServer.fromJson(jsonObject));
+                        break;
+                }
+            });
+
+            callback.accept(returnList);
+
         }));
     }
 
